887ed315b09d2f62713e31803e8c0c46cb1c4984
Bunny4242 <pannipa.ko.64@ubu.ac.th>
2022-09-06 14:55:56 +0700

    update4

--- Assets/Scrips/S1.cs
+++ Assets/Scrips/S1.cs
@@ -13,20 +13,26 @@ public class S1 : MonoBehaviour
     Text textDisplay;
     int currentLine = 0;
     TW_MultiStrings_Regular tw;
+    FadeCanvas fadeCanvas;
 
 
 
     void Awake()
     {
         string allText = textFile.text;
+        //line = allText.Split("\n");
         textDisplay = GameObject.Find("Text").GetComponent<Text>();
-        //textDisplay.text = line[currentLine];
         tw = GameObject.Find("Text").GetComponent<TW_MultiStrings_Regular>();
+        fadeCanvas = GameObject.Find("Canvas").GetComponent<FadeCanvas>();
         tw.MultiStrings = allText.Split("\n");
         textDisplay.text = tw.MultiStrings[0];
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        fadeCanvas.ShowUI();
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -36,22 +42,20 @@ public class S1 : MonoBehaviour
             {
                 if (SceneManager.GetActiveScene().name.CompareTo("S1") == 0)
                 {
-                    SceneManager.LoadScene("S2");
+                    fadeCanvas.HideUI();
                 }
-                
             }
             else tw.NextString();
-            
         }
-        
     }
+
     void displayText()
     {
         string[] tmp = new string[2];
         tmp = line[currentLine].Split("//");
         int cNumber = int.Parse(tmp[0]);
-        string txt = "";
-        if (cNumber > 0) txt = names[cNumber] + " : " + tmp[1];
+        string txt = " ";
+        if (cNumber > 0) txt = names[cNumber] + "//" + tmp[1];
         else txt = tmp[1];
         textDisplay.text = txt;
     }
