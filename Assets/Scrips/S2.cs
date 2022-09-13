a100fe9aae537a0ef8be380c9a59b046bff6e685
Bunny4242 <pannipa.ko.64@ubu.ac.th>
2022-09-13 16:00:34 +0700

    ..

---
+++ Assets/Scrips/S2.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class S2 : MonoBehaviour
+{
+    public TextAsset textFile;
+    string[] line = new string[10];
+    string[] names = { "MIRUN", "ROSE" };
+    Text textDisplay , textName;
+    int currentLine = 0;
+    TW_MultiStrings_Regular tw;
+    FadeCanvas fadeCanvas;
+    bool showChoices = false;
+    public GameObject choicesPanel;
+
+
+
+    void Awake()
+    {
+        string allText = textFile.text;
+        line = allText.Split("\n");
+        textDisplay = GameObject.Find("Text").GetComponent<Text>();
+        textName = GameObject.Find("TextName").GetComponent<Text>();
+        tw = GameObject.Find("Text").GetComponent<TW_MultiStrings_Regular>();
+        fadeCanvas = GameObject.Find("Canvas").GetComponent<FadeCanvas>();
+        tw.MultiStrings = allText.Split("\n");
+        displayName();
+    }
+
+    void Start()
+    {
+        fadeCanvas.ShowUI();
+    }
+
+    void Update()
+    {
+        if (currentLine == 9) showChoices = true;
+        if (Input.GetMouseButtonDown(0) && showChoices == false)
+        {
+            currentLine++;
+            if (currentLine >= 10)
+            {
+                if (SceneManager.GetActiveScene().name.CompareTo("S1") == 0)
+                {
+                    fadeCanvas.HideUI();
+                }
+            }
+            else
+            {
+                displayName();
+                tw.NextString();
+            }
+        }
+        if (showChoices == true)
+        {
+            choicesPanel.SetActive(true);
+        }
+    }
+
+    void displayName()
+    {
+        string[] tmp = new string[2];
+        tmp = tw.MultiStrings[currentLine].Split(":");
+        tw.MultiStrings[currentLine] = tmp[1];
+        int cNumber = int.Parse(tmp[0]);
+        textName.text = names[cNumber];
+        if (currentLine == 0) textDisplay.text = tmp[1];
+        
+    }
+}
